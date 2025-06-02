3783abf189bded4376c65d91dbcd676b2ac8031b
HanninenF <hanninenfredrik@gmail.com>
2025-06-02 23:32:44 +0200

    add: implement pictures to mockData

--- app/data/mockData.ts
+++ app/data/mockData.ts
@@ -5,7 +5,8 @@ export const monsterUsers: MonsterType[] = [
   {
     id: "1",
     userName: "Jan-Olof",
-    picSource: "23",
+    picSource:
+      "https://media.istockphoto.com/id/1445781372/sv/foto/tiger-looking-at-the-camera-on-a-black-background.jpg?s=2048x2048&w=is&k=20&c=GmfhJXx7HYuxo1vPk8dS0vntF42J_sQ14deAKn7Gc_E=",
   },
 ];
 
@@ -14,7 +15,8 @@ export const monsterPosts: PostType[] = [
   {
     monsterUser: monsterUsers[0],
     postedAt: Date(),
-    postImageUrl: "23",
+    postImageUrl:
+      "https://focus.independent.ie/thumbor/hw8MRpqWDc-IKK3L7nvbTf3egzY=/0x0:2500x1665/960x640/prod-mh-ireland/1ea19a34-c468-11ed-a8bd-0210609a3fe2",
     text: "jajaja",
   },
 ];
