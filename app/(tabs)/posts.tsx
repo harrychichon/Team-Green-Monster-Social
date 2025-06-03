ca758132a1ece463a136cbee1daae0b207735d98
greenwinther <dennisgw@live.se>
2025-06-03 11:42:46 +0200

    style: standardize quotes and formatting in Posts component

--- app/(tabs)/posts.tsx
+++ app/(tabs)/posts.tsx
@@ -1,44 +1,44 @@
-import { StyleSheet, ScrollView, View } from 'react-native';
-import PostComponent from './../components/posts/PostComponent';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import SwitchUserButton from '../components/buttons/switchUserButton';
-import AddPostButton from '../components/buttons/addPostButton';
+import { StyleSheet, ScrollView, View } from "react-native";
+import PostComponent from "./../components/posts/PostComponent";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AddPostButton from "../components/buttons/addPostButton";
+import SwitchUserButton from "../components/buttons/SwitchUserButton";
 
 export default function Posts() {
-  return (
-    <SafeAreaProvider style={styles.safeArea}>
-      <View style={styles.topButtonContainer}>
-        <SwitchUserButton />
-      </View>
+	return (
+		<SafeAreaProvider style={styles.safeArea}>
+			<View style={styles.topButtonContainer}>
+				<SwitchUserButton />
+			</View>
 
-      <ScrollView style={styles.scrollView}>
-        <PostComponent />
-      </ScrollView>
+			<ScrollView style={styles.scrollView}>
+				<PostComponent />
+			</ScrollView>
 
-      <View style={styles.addButtonContainer}>
-        <AddPostButton />
-      </View>
-    </SafeAreaProvider>
-  );
+			<View style={styles.addButtonContainer}>
+				<AddPostButton />
+			</View>
+		</SafeAreaProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#1e1e1e',
-    paddingTop: 25,
-  },
-  topButtonContainer: {
-    alignItems: 'flex-end',
-    paddingHorizontal: 20,
-    marginBottom: 10,
-  },
-  scrollView: {
-    paddingHorizontal: 20,
-    backgroundColor: '#1e1e1e',
-  },
-  addButtonContainer: {
-    alignItems: 'center',
-    marginVertical: 20,
-  },
+	safeArea: {
+		flex: 1,
+		backgroundColor: "#1e1e1e",
+		paddingTop: 25,
+	},
+	topButtonContainer: {
+		alignItems: "flex-end",
+		paddingHorizontal: 20,
+		marginBottom: 10,
+	},
+	scrollView: {
+		paddingHorizontal: 20,
+		backgroundColor: "#1e1e1e",
+	},
+	addButtonContainer: {
+		alignItems: "center",
+		marginVertical: 20,
+	},
 });
