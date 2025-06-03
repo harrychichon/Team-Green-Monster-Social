d726bd00aa0fb555d63697fc20a6aad58cd737d4
Harry Chichon <harry.chichon@proton.me>
2025-06-03 12:40:08 +0200

    Added centralised styling and an instructions file.

---
+++ app/theme/radius.ts
@@ -0,0 +1,6 @@
+export const RADIUS = {
+	sm: 4,
+	md: 8,
+	lg: 16,
+	full: 9999,
+};
---
+++ app/theme/shadows.ts
@@ -0,0 +1,23 @@
+export const SHADOW = {
+	light: {
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 1 },
+		shadowOpacity: 0.1,
+		shadowRadius: 2,
+		elevation: 2,
+	},
+	medium: {
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.2,
+		shadowRadius: 4,
+		elevation: 4,
+	},
+	heavy: {
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 4 },
+		shadowOpacity: 0.3,
+		shadowRadius: 8,
+		elevation: 8,
+	},
+};
