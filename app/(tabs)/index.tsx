301c23ed1fb7acdd3fb6d2751456599577bea373
greenwinther <dennisgw@live.se>
2025-06-03 13:56:50 +0200

    refactor: streamline user selection process and enhance state management

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,71 +6,78 @@ import { useRouter } from "expo-router";
 import useSocialContext from "../hooks/useSocialContext";
 
 export default function Index() {
-  const { monsters } = useSocialContext();
-  const router = useRouter();
-  const borderColors = ["red", "green", "blue"];
+	const { monsters } = useSocialContext();
+	const { setCurrentUser } = useSocialContext();
+	const router = useRouter();
+	const borderColors = ["red", "green", "blue", "yellow", "purple", "orange"];
 
-  return (
-    <SafeAreaProvider style={styles.container}>
-      <Text style={styles.header}>Choose user</Text>
-      <View style={styles.profileRow}>
-        {monsters.map((user, index) => {
-          const borderColor = borderColors[index % borderColors.length];
-          return (
-            <TouchableOpacity
-              key={user.id}
-              style={styles.profileContainer}
-              onPress={() =>
-                router.push({
-                  pathname: "/(tabs)/posts",
-                  params: { user: JSON.stringify(user) },
-                })
-              }
-            >
-              <Image
-                source={{ uri: user.picSource }}
-                style={[styles.profileImage, { borderColor }]}
-              />
-              <Text style={styles.profileName}>{user.userName}</Text>
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-    </SafeAreaProvider>
-  );
+	const submitHandle = (id: string) => {
+		const selectedUser = monsters.find((user: MonsterType) => user.id === id);
+		if (selectedUser) {
+			setCurrentUser(selectedUser);
+			console.log("Selected User:", selectedUser);
+			router.push("/(tabs)/posts");
+		}
+	};
+
+	return (
+		<SafeAreaProvider style={styles.container}>
+			<Text style={styles.header}>Choose user</Text>
+			<View style={styles.profileRow}>
+				{monsters.map((user, index) => {
+					const borderColor = borderColors[index % borderColors.length];
+					return (
+						<TouchableOpacity
+							key={user.id}
+							style={styles.profileContainer}
+							onPress={() => {
+								submitHandle(user.id);
+							}}
+						>
+							<Image
+								source={{ uri: user.picSource }}
+								style={[styles.profileImage, { borderColor }]}
+							/>
+							<Text style={styles.profileName}>{user.userName}</Text>
+						</TouchableOpacity>
+					);
+				})}
+			</View>
+		</SafeAreaProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+	container: {
+		flex: 1,
 
-    backgroundColor: "#000",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  header: {
-    color: "#fff",
-    fontSize: 24,
-    marginBottom: 40,
-  },
-  profileRow: {
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-  profileContainer: {
-    alignItems: "center",
-    marginHorizontal: 10,
-  },
-  profileImage: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-    borderWidth: 3,
-    marginBottom: 10,
-  },
-  profileName: {
-    fontSize: 12,
-    letterSpacing: 1,
-    color: "white",
-  },
+		backgroundColor: "#000",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	header: {
+		color: "#fff",
+		fontSize: 24,
+		marginBottom: 40,
+	},
+	profileRow: {
+		flexDirection: "row",
+		justifyContent: "center",
+	},
+	profileContainer: {
+		alignItems: "center",
+		marginHorizontal: 10,
+	},
+	profileImage: {
+		width: 80,
+		height: 80,
+		borderRadius: 40,
+		borderWidth: 3,
+		marginBottom: 10,
+	},
+	profileName: {
+		fontSize: 12,
+		letterSpacing: 1,
+		color: "white",
+	},
 });
